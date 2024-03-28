9cf15c1a60f0843f71742ef93d25221d4ef20a36
kimho <rlaghekd777@naver.com>
2024-03-29 00:03:52 +0900

    # ADD - spring config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = 'http://localhost:8080';
+// export const BASE_URL = 'http://localhost:8080';
+// export const BASE_URL = 'https://api.kp-realworld.com';
+
+export const BASE_URL = 'https:/test.kp-realworld.com';
