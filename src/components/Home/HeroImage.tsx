575e234513f879a47b060b5f76016e0cd5b1f8dc
hotkimho <rlaghekd777@naver.com>
2024-04-14 04:46:24 +0900

    # EDIT - banner image

--- src/components/Home/HeroImage.tsx
+++ src/components/Home/HeroImage.tsx
@@ -3,16 +3,17 @@ import React from 'react';
 
 const HeroImage: React.FC = () => {
     return (
-        <div className="bg-white hero min-h-screen"  style={{ backgroundImage: `url('')` }}>
-            <div className="hero-overlay bg-opacity-60"></div>
+        <div className="bg-green-500 hero h-64" style={{ backgroundImage: `url('')` }}>
+            <div className="hero-overlay bg-opacity-10"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">Welcome to Conduit</p>
+                    <h1 className="mb-5 text-5xl font-bold">conduit</h1>
+                    <p className="mb-5">A place to share your knowledge.</p>
                 </div>
             </div>
         </div>
     );
 };
+
 
 export default HeroImage;
